df9c5408881c70466997c2163a6bca8086ab166f
Jack Tattersall <jacktattersall1@gmail.com>
2016-05-27 11:34:22 +0100

    Added film class shell

---
+++ FilmStore.core/Film.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmStore.core
+{
+    public class Film
+    {
+        public Film()
+        {
+
+        }
+
+        public Film(string title, DateTime released, int stock, Enum genre)
+        {
+            
+        }
+
+        public long id { get; set; }
+        public string Title { get; set; }
+        public DateTime Released { get; set; }
+        public int Stock { get; set; }
+        public Enum Genre { get; set; }
+    }
+}
