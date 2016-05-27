13083d526b4752375bddf216d85782cccc787881
Jack Tattersall <jacktattersall1@gmail.com>
2016-05-27 13:59:19 +0100

    Added context class

---
+++ FilmStore.core/FilmStoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using FilmStore.core.Properties;
+
+namespace FilmStore.core
+{
+    class FilmStoreContext : DbContext
+    {
+        public FilmStoreContext() : base(new Settings().connectionString)
+        {
+
+        }
+        public DbSet<Film> Films { get; set; }
+    }
+}
