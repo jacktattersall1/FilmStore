e3c50aa67e8f9f53bbe72406d934af4fa9bd34d0
Jack Tattersall <jacktattersall1@gmail.com>
2016-05-27 12:39:31 +0100

    Added FilmRepositoryDb

---
+++ FilmStore.core/FilmRepositoryDB.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmStore.core
+{
+    class FilmRepositoryDB : IFilmRepository
+    {
+        public bool Delete(Film film)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long Insert(Film film)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Film> SelectAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Film SelectById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Film SelectByTitle(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Film film)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
