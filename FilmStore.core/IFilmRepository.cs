2c4df766de2c9a85e1ca7263684f4a008aa0c50d
Jack Tattersall <jacktattersall1@gmail.com>
2016-05-27 12:36:41 +0100

    Added IFilmRepository

---
+++ FilmStore.core/IFilmRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmStore.core
+{
+    interface IFilmRepository
+    {
+        long Insert(Film film);
+        Film SelectById(long id);
+        ICollection<Film> SelectAll();
+        Film SelectByTitle(string title);
+        bool Update(Film film);
+        bool Delete(Film film);
+    }
+}
