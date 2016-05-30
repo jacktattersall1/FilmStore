ec7ea22b8ad35a50c4fe5337770b769045f8684c
Jack Tattersall <jacktattersall1@gmail.com>
2016-05-30 14:23:08 +0100

    Added Serializer integration tests for CollectionFilmRepository

---
+++ FilmStore.IntegrationTests/CollectionFilmRepositoryTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using FilmStore.core;
+
+namespace FilmStore.IntegrationTests
+{
+    [TestClass]
+    public class CollectionFilmRepositoryTest
+    {
+        [ClassInitialize]
+        public static void RunOnceForAllTests(TestContext context)
+        {
+            string path = @"C:\Users\jackt\OneDrive\Documents\FilmStoreSeralizes\object.xml";
+            File.Delete(path);
+        }
+
+        [TestMethod]
+        public void CollectionFilmRepository_IT()
+        {
+            //Arrange
+            CollectionFilmRepository sut = new CollectionFilmRepository(new Serializer());
+            Film film1 = new Film("Aliens", new DateTime(1984, 1, 20), 5, Genre.Science_Fiction);
+            Film film2 = new Film("Predator", new DateTime(1984, 1, 20), 5, Genre.Science_Fiction);
+
+            //Act
+            long id1 = sut.Insert(film1);
+            long id2 = sut.Insert(film2);
+
+            //Assert
+            Assert.AreEqual(2, sut.SelectAll().Count, "Assertion 1");
+            Assert.AreEqual(1, sut.SearchByTitle("Aliens").Count, "Assertion 2");
+            Assert.AreEqual(film1, sut.SelectById(id1), "Assertion 3");
+            Assert.AreEqual(film2, sut.SelectById(id2), "Assertion 4");
+
+            film1.Stock = 0;
+            sut.Update(film1);
+            Assert.AreEqual(0, sut.SelectById(id1).Stock, "Assertion 5");
+
+            sut.Delete(film2);
+            Assert.IsNull(sut.SelectById(id2), "Assertion 6");
+        }
+    }
+}
---
+++ FilmStore.core/Serializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using FilmStore.core.Interfaces;
+using System.IO;
+using System.Runtime.Serialization;
+
+namespace FilmStore.core
+{
+    public class Serializer : ISerializer
+    {
+        private string path = @"C:\Users\jackt\OneDrive\Documents\FilmStoreSeralizes\object.xml";
+
+        public ICollection<Film> Read()
+        {
+            if (!File.Exists(path))
+                return new HashSet<Film>();
+
+            using(FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                var serializer = new NetDataContractSerializer();
+            }
+        }
+
+        public void Write(ICollection<Film> films)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
