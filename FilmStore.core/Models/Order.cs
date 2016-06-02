387d75a598f24b9372f0c1a467db7fc81ef9c2bc
Jack Tattersall <jacktattersall1@gmail.com>
2016-06-02 20:58:21 +0100

    Added Order class implements IOrder

---
+++ FilmStore.core/Models/Order.cs
@@ -0,0 +1,30 @@
+﻿using FilmStore.core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmStore.core.Models
+{
+    class Order : IOrder
+    {
+        public List<Film> Films { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public int OrderId { get; set; }
+
+        public string UserName { get; set; }
+
+        public bool AddFilm(Film film)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveFilm(Film film)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
