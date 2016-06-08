3423a84869005e1dba7c989b00d92aa08676637e
Jack Tattersall <jacktattersall1@gmail.com>
2016-06-08 19:11:50 +0100

    Added a new MVC project and Test Project for it

---
+++ FilmStore.Portal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FilmStore.Portal.MvcApplication" Language="C#" %>
---
+++ FilmStore.Portal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FilmStore.Portal.Startup))]
+namespace FilmStore.Portal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
